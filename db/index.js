70501e65d8359d20c4300ecdc2c64ccff8deff12
Anton <a.alexeyev@mail.ru>
2019-06-25 09:50:23 +0300

    Улучшена работа с БД

--- db/index.js
+++ db/index.js
@@ -12,62 +12,108 @@ class DB {
     return path.join(process.cwd(), this.file);
   }
   init (data) {
-    fs.writeFileSync(this._fullnameFile(), JSON.stringify(data, '', 4));
+    return new Promise((resolve, reject) => {
+      try {
+        fs.writeFileSync(this._fullnameFile(), JSON.stringify(data, '', 4));
+        resolve(true);
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
-  find (type) {
-    let data = [];
-    if (fs.existsSync(this._fullnameFile())) {
-      data = JSON.parse(fs.readFileSync(this._fullnameFile(), 'utf-8'))[type];
-    }
-    return data;
+  get (type) {
+    return new Promise((resolve, reject) => {
+      try {
+        let data = [];
+        if (fs.existsSync(this._fullnameFile())) {
+          data = JSON.parse(fs.readFileSync(this._fullnameFile(), 'utf-8'))[type];
+        }
+        resolve(data);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+  find (type, field, value) {
+    return new Promise((resolve, reject) => {
+      try {
+        let data = [];
+        if (fs.existsSync(this._fullnameFile())) {
+          data = JSON.parse(fs.readFileSync(this._fullnameFile(), 'utf-8'))[type];
+        }
+        const item = data.find(elem => elem[field] === value)[field];
+        resolve(item);
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
   add (type, data) {
-    if (type === 'products') {
-      // отработаем только добавление products, для данной задачи skills не нужно
-      let currentData = JSON.parse(fs.readFileSync(this._fullnameFile(), 'utf-8'));
-      const { photoName, name, price } = data;
-      let newProducts = currentData[type];
-      newProducts.push({
-        'src': './assets/img/products/' + photoName,
-        'name': name,
-        'price': price
-      });
-      currentData[type] = newProducts;
-      fs.writeFileSync(this._fullnameFile(), JSON.stringify(currentData, '', 4));
-    }
+    return new Promise((resolve, reject) => {
+      try {
+        if (type === 'products') {
+          // отработаем только добавление products, для данной задачи skills не нужно
+          let currentData = JSON.parse(fs.readFileSync(this._fullnameFile(), 'utf-8'));
+          const { photoName, name, price } = data;
+          let newProducts = currentData[type];
+          newProducts.push({
+            'src': './assets/img/products/' + photoName,
+            'name': name,
+            'price': price,
+            'id': newProducts.length + 1
+          });
+          currentData[type] = newProducts;
+          fs.writeFileSync(this._fullnameFile(), JSON.stringify(currentData, '', 4));
+        }
+        resolve(true);
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
   // delete (type, data) {} для данной задачи реализовывать не нужно
   // get (type, data) {} для данной задачи реализовывать не нужно
   update (type, data) {
-    if (type === 'skills') {
-      // отработаем только редактирование skills, для данной задачи products не нужно
-      let currentData = JSON.parse(fs.readFileSync(this._fullnameFile(), 'utf-8'));
-      const { age, concerts, cities, years } = data;
-      let newSkills = [];
-      newSkills.push(
-        {
-          number: age,
-          text: 'Возраст начала занятий на скрипке'
-        },
-        {
-          number: concerts,
-          text: 'Концертов отыграл'
-        },
-        {
-          number: cities,
-          text: 'Максимальное число городов в туре'
-        },
-        {
-          number: years,
-          text: 'Лет на сцене в качестве скрипача'
+    return new Promise((resolve, reject) => {
+      try {
+        if (type === 'skills') {
+          // отработаем только редактирование skills, для данной задачи products не нужно
+          let currentData = JSON.parse(fs.readFileSync(this._fullnameFile(), 'utf-8'));
+          const { age, concerts, cities, years } = data;
+          let newSkills = [];
+          newSkills.push(
+            {
+              number: age,
+              text: 'Возраст начала занятий на скрипке',
+              id: 1
+            },
+            {
+              number: concerts,
+              text: 'Концертов отыграл',
+              id: 2
+            },
+            {
+              number: cities,
+              text: 'Максимальное число городов в туре',
+              id: 3
+            },
+            {
+              number: years,
+              text: 'Лет на сцене в качестве скрипача',
+              id: 4
+            }
+          );
+          currentData[type] = newSkills;
+          fs.writeFileSync(
+            this._fullnameFile(),
+            JSON.stringify(currentData, '', 4)
+          );
         }
-      );
-      currentData[type] = newSkills;
-      fs.writeFileSync(
-        this._fullnameFile(),
-        JSON.stringify(currentData, '', 4)
-      );
-    }
+        resolve(true);
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
 }
 
@@ -75,25 +121,25 @@ const db = new DB('/temp/database.json');
 db.init({
   products: [],
   skills: [
-    { number: '67', text: 'Возраст начала занятий на скрипке' },
-    { number: '45', text: 'Концертов отыграл' },
-    { number: '32', text: 'Максимальное число городов в туре' },
-    { number: '11', text: 'Лет на сцене в качестве скрипача' }
+    { number: '67', text: 'Возраст начала занятий на скрипке', id: 1 },
+    { number: '45', text: 'Концертов отыграл', id: 2 },
+    { number: '32', text: 'Максимальное число городов в туре', id: 3 },
+    { number: '11', text: 'Лет на сцене в качестве скрипача', id: 4 }
   ]
 });
 
-DATABASE.on('products/get', response => {
-  response.reply(db.find('products'));
+DATABASE.on('products/get', async response => {
+  response.reply(await db.get('products'));
 });
 
-DATABASE.on('skills/get', response => {
-  response.reply(db.find('skills'));
+DATABASE.on('skills/get', async response => {
+  response.reply(await db.get('skills'));
 });
 
-DATABASE.on('products/post', response => {
-  response.reply(db.add('products', response.data));
+DATABASE.on('products/post', async response => {
+  response.reply(await db.add('products', response.data));
 });
 
-DATABASE.on('skills/post', response => {
-  response.reply(db.update('skills', response.data));
+DATABASE.on('skills/post', async response => {
+  response.reply(await db.update('skills', response.data));
 });
